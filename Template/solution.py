2052db3c8a7a70f326814e4877dd031bb0d335c0
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-29 21:15:42 +0200

    Adding Template

---
+++ Template/solution.py
@@ -0,0 +1,6 @@
+def solution(A):
+    return A
+
+
+if __name__=="__main__":
+    solution("")
---
+++ Template/test.py
@@ -0,0 +1,6 @@
+from solution import solution
+
+def test_solution():
+    input1 = []
+    print("test1: {} => {}".format(input1, []))
+    assert solution(input1) == []
